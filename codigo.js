bfa81e72910ccec866bb2ec1311d07aecb6de71d
priscilapenela <priscila.penela@gmail.com>
2023-06-20 22:09:18 -0300

    correcciones

--- codigo.js
+++ codigo.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("#formulario");
+const lista = document.getElementById("lista")
 const inputs = document.querySelectorAll('#formulario input');
 const button = document.querySelector('#formulario button');
 const ArrayDatos = [];
@@ -41,8 +42,10 @@ const validarCampo = (expresion, input, campo, message) => {
         input.nextElementSibling.classList.remove("invalid-feedback");
         input.nextElementSibling.innerText = "";
         campos[campo] = true;
-        if (document.getElementById('aviso')) {
-            document.getElementById('aviso').remove();
+        if (document.getElementsByClassName('alert')) {
+            document.getElementById('send').nextElementSibling.classList.remove('alert');
+            document.getElementById('send').nextElementSibling.removeAttribute('role');
+            document.getElementById('send').nextElementSibling.innerText = "";
         }
 
     } else {
@@ -58,7 +61,6 @@ const guardarDatos = (e) => {
     const empresaNombre = document.getElementById("inputenterprise").value;
     const sueldo = document.getElementById("inputsalary").value;
     const email = document.getElementById("inputemail").value;
-    const nuevaEtiqueta = document.createElement('div');
     e.preventDefault();
 
     if (campos.inputname && campos.inputenterprise && campos.inputsalary && campos.inputemail) {
@@ -72,90 +74,97 @@ const guardarDatos = (e) => {
 
         if (sueldo > 100000) {
             ArrayDatos.push(Dato);
-            crearTabla(Dato);
+            ArrayDatos.sort((a, b) => b.dato3 - a.dato3);
         }
 
         form.reset()
+
+        if(document.getElementById('table')){
+            document.getElementById('botonLista').nextElementSibling.remove();
+        }
+        if(document.getElementsByClassName('alert')){
+            document.getElementById('lista').nextElementSibling.classList.remove('alert');
+            document.getElementById('lista').nextElementSibling.removeAttribute('role');
+            document.getElementById('lista').nextElementSibling.innerText = "";
+        }
+
     } else {
-        document.getElementById('send').insertAdjacentElement('afterend', nuevaEtiqueta);
-        document.getElementById('send').nextElementSibling.classList.add("alert", "alert-dark");
+        document.getElementById('send').nextElementSibling.classList.add('alert', 'alert-dark');
         document.getElementById('send').nextElementSibling.setAttribute('role', 'alert');
-        document.getElementById('send').nextElementSibling.setAttribute('id', 'aviso')
-        document.getElementById('send').nextElementSibling.innerText = "El formulario no ha sido completado correctamente";
+        document.getElementById('send').nextElementSibling.innerText = 'El formulario no ha sido completado correctamente';
     }
 }
 
-const crearTabla = (obj) => {
-    const posicion = document.getElementById('formulario');
+const crearTabla = (e) => {
+    const posicion = document.getElementById('botonLista');
     const divTabla = document.createElement('div');
-    const tabla = document.createElement('table');
-    tabla.classList.add("table", "table-stripped");
-    const dolar = 245;
-
-    if (document.getElementById('table')) {
-        const t = document.getElementById('table');
-
-        let fila = t.insertRow(-1);
-
-        let celda = fila.insertCell(0);
-        celda.textContent = obj.dato1;
-
-        celda = fila.insertCell(1);
-        celda.textContent = obj.dato2;
-
-        celda = fila.insertCell(2);
-        celda.textContent = obj.dato3;
-
-        celda = fila.insertCell(3);
-        celda.textContent = (obj.dato3) / dolar;
-
-        celda = fila.insertCell(4);
-        celda.textContent = obj.dato4;
-
-
-    } else {
-        posicion.insertAdjacentElement('afterend', divTabla).classList.add("ofertas");
-        divTabla.insertAdjacentElement("afterbegin", tabla).setAttribute("id", "table");
-        let t = document.getElementById('table');
-        t.style.marginLeft = "auto";
-        let fila = tabla.insertRow(0);
-
-        let celda = fila.insertCell(0);
-        celda.textContent = "Nombre y Apellido";
-
-        celda = fila.insertCell(1);
-        celda.textContent = "Empresa";
-
-        celda = fila.insertCell(2);
-        celda.textContent = "Sueldo";
-
-        celda = fila.insertCell(3);
-        celda.textContent = "Sueldo en Dolares";
-
-        celda = fila.insertCell(4);
-        celda.textContent = "Email";
-
-        fila = t.insertRow(-1);
-
-        celda = fila.insertCell(0);
-        celda.textContent = obj.dato1;
-
-        celda = fila.insertCell(1);
-        celda.textContent = obj.dato2;
-
-        celda = fila.insertCell(2);
-        celda.textContent = obj.dato3;
-
-        celda = fila.insertCell(3);
-        celda.textContent = (obj.dato3) / dolar;
-
-        celda = fila.insertCell(4);
-        celda.textContent = obj.dato4;
-
+    
+    if(ArrayDatos.length == 0){
+        document.getElementById('lista').nextElementSibling.classList.add("alert", "alert-dark");
+        document.getElementById('lista').nextElementSibling.setAttribute('role', 'alert');
+        document.getElementById('lista').nextElementSibling.innerText = "No se han enviado datos";
+    }else{
+        if(document.getElementById('table')){
+            document.getElementById('lista').nextElementSibling.classList.add("alert", "alert-dark");
+            document.getElementById('lista').nextElementSibling.setAttribute('role', 'alert');
+            document.getElementById('lista').nextElementSibling.innerText = "Lista creada";
+        }else{
+    
+            const tabla = document.createElement('table');
+            tabla.classList.add("table", "table-striped-columns");
+            const dolar = 245;
+            e.preventDefault();
+        
+            posicion.insertAdjacentElement('afterend', divTabla).classList.add("ofertas");
+            divTabla.insertAdjacentElement('afterbegin', tabla).setAttribute("id", "table");
+            let t = document.getElementById('table');
+            let d = document.getElementsByClassName("ofertas");
+            divTabla.style.margin = "auto";
+            t.style.marginLeft = "auto";
+            let fila = tabla.insertRow(0);
+        
+            let celda = fila.insertCell(0);
+            celda.textContent = "Nombre y Apellido";
+        
+            celda = fila.insertCell(1);
+            celda.textContent = "Empresa";
+        
+            celda = fila.insertCell(2);
+            celda.textContent = "Sueldo";
+        
+            celda = fila.insertCell(3);
+            celda.textContent = "Sueldo en Dolares";
+        
+            celda = fila.insertCell(4);
+            celda.textContent = "Email";
+        
+            for(let i = 0; i<ArrayDatos.length; i++){
+                const obj = ArrayDatos[i];
+                fila = t.insertRow(-1);
+        
+                celda = fila.insertCell(0);
+                celda.textContent = obj.dato1;
+        
+                celda = fila.insertCell(1);
+                celda.textContent = obj.dato2;
+        
+                celda = fila.insertCell(2);
+                celda.textContent = obj.dato3;
+        
+                celda = fila.insertCell(3);
+                celda.textContent = (obj.dato3) / dolar;
+        
+                celda = fila.insertCell(4);
+                celda.textContent = obj.dato4;
+                
+            }
+        }
     }
-
 }
 
+
+
+lista.addEventListener('click', crearTabla);
 form.addEventListener('submit', guardarDatos);
 
 inputs.forEach((input) => {
